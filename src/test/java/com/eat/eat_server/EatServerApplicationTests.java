a4ab9ae0268fee157137ddef9d2c30f928bf6c38
yunji118 <yunji118@naver.com>
2024-01-17 19:31:43 +0900

    project init

---
+++ src/test/java/com/eat/eat_server/EatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eat.eat_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
