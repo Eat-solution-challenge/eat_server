f8d52c00c39e13bc25e7f53173fc69cc9730f954
yunji118 <yunji118@naver.com>
2024-02-02 03:06:02 +0900

    [feat]user entity 생성 #1

--- src/main/java/Entity/User.java
+++ src/main/java/Entity/User.java
@@ -1,2 +1,31 @@
-package Entity;public class User {
+package Entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class User extends BaseTimeEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String nickname;
+
+    private String email;
+
+    private String password;
+
+    private int age;
+
+    private String gender;
+
+    private Long height;
+
+    private Long weight;
+
 }
