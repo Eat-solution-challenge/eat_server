dd977893cc8115738f77fe3a95462a8910b5dc50
yunji118 <yunji118@naver.com>
2024-02-02 03:18:21 +0900

    [feat] userRepo, usercontroller 생성 #1

---
+++ src/main/java/com/eat/eat_server/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.eat.eat_server.Repository;
+
+import com.eat.eat_server.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
