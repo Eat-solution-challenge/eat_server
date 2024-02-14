3a6cacbfa12bc77eb9bd687f8e5a26337bbb37b0
yunji118 <yunji118@naver.com>
2024-02-15 06:27:51 +0900

    [feat]적정량 계산 로직 수정 #2

--- src/main/java/com/eat/eat_server/domain/logs/service/ProperService.java
+++ src/main/java/com/eat/eat_server/domain/logs/service/ProperService.java
@@ -1,8 +1,10 @@
 package com.eat.eat_server.domain.logs.service;
 
 import com.eat.eat_server.domain.logs.domain.Log;
+import com.eat.eat_server.domain.logs.domain.SubCategory;
 import com.eat.eat_server.domain.logs.dto.ProperAmountDto;
 import com.eat.eat_server.domain.logs.repository.LogRepository;
+import com.eat.eat_server.domain.logs.repository.SubCategoryRepository;
 import com.eat.eat_server.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,9 +15,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProperService {
     private final LogRepository logRepository;
+    private final SubCategoryRepository subCategoryRepository;
 
-    public ProperAmountDto getProperAmount(User user, Long subCategoryId) {
-        List<Log> logs = logRepository.findBySubCategoryId(subCategoryId);
+    public ProperAmountDto getProperAmount(User user, String subCategoryName) {
+
+        SubCategory subCategory = subCategoryRepository.findByUserAndName(user, subCategoryName);
+
+        List<Log> logs = logRepository.findBySubCategoryId(subCategory.getId());
         int sumOfCalorie = 0;
         for(Log l:logs){
             if (l.getLevel().equals("LEVEL_LIGHT"))
