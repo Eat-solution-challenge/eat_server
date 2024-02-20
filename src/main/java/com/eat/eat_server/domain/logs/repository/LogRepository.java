da4e1d7084219b800c6fc70d21fcee58cede8190
doyeoo <81256252+doyeoo@users.noreply.github.com>
2024-02-21 00:10:52 +0900

    Revert "[feat] 캘린더 로그 조회, 특정 메뉴 로그 검색"

--- src/main/java/com/eat/eat_server/domain/logs/repository/LogRepository.java
+++ src/main/java/com/eat/eat_server/domain/logs/repository/LogRepository.java
@@ -1,10 +1,7 @@
 package com.eat.eat_server.domain.logs.repository;
 
 import com.eat.eat_server.domain.logs.domain.Log;
-import com.eat.eat_server.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,6 +9,4 @@ import java.util.List;
 public interface LogRepository extends JpaRepository<Log, Long> {
 
     List<Log> findBySubCategoryId(long subCategoryId);
-    @Query("SELECT l FROM Log l WHERE l.menu LIKE %:keyword% AND l.subCategory.user = :user")
-    List<Log> findByUserAndMenuKeyword(@Param("user") User user, @Param("keyword") String keyword);
 }
