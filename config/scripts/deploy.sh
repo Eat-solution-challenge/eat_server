d801a88f15d193de53a2ffd4039ffeb71ab2ba84
doyeoo <81256252+doyeoo@users.noreply.github.com>
2024-02-25 12:41:41 +0900

    Update deploy.sh

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -35,5 +35,5 @@ sudo docker-compose down
 echo "start docker-compose up"
 sudo docker-compose -f /home/ubuntu/srv/ubuntu/docker-compose.yml up --build -d
 
-echo "remove unused images"
-sudo docker image prune -a
+echo "remove unused resources"
+sudo docker system prune -af
